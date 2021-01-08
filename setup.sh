d3951c1ec14b61aec6d36457e177b3d5cabdcc5a
Antonio Caputo <lactobacillo@gmail.com>
2021-01-08 18:34:02 +0100

    add setup file

---
+++ setup.sh
@@ -0,0 +1,161 @@
+echo "Welcome! Let's start setting up your system. It could take more than 10 minutes, be patient"
+
+echo "What name do you want to use in GIT user.name?"
+echo "For example, mine will be \"Luke Morales\""
+read git_config_user_name
+
+echo "What email do you want to use in GIT user.email?"
+echo "For example, mine will be \"lukemorales@live.com\""
+read git_config_user_email
+
+echo "What is your github username?"
+echo "For example, mine will be \"lukemorales\""
+read username
+
+cd ~ && sudo apt-get update
+
+echo 'Installing curl' 
+sudo apt-get install curl -y
+
+echo 'Installing neofetch' 
+sudo apt-get install neofetch -y
+
+echo 'Installing tool to handle clipboard via CLI'
+sudo apt-get install xclip -y
+
+echo 'Installing latest git' 
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update && sudo apt-get install git -y
+
+echo 'Installing python3-pip'
+sudo apt-get install python3-pip -y
+
+echo 'Installing getgist to download dot files from gist'
+sudo pip3 install getgist
+export GETGIST_USER=$username
+
+if [$XDG_CURRENT_DESKTOP == 'KDE'] ; then
+    echo 'Cloning your Konsole configs from gist'
+    cd ~/.local/share/konsole && getmy OmniKonsole.profile && getmy OmniTheme.colorscheme
+
+    echo 'Installing Latte Dock'
+    sudo add-apt-repository ppa:kubuntu-ppa/backports -y
+    sudo apt-get update && sudo apt-get dist-upgrade
+    sudo apt-get install cmake extra-cmake-modules qtdeclarative5-dev libqt5x11extras5-dev libkf5iconthemes-dev libkf5plasma-dev libkf5windowsystem-dev libkf5declarative-dev libkf5xmlgui-dev libkf5activities-dev build-essential libxcb-util-dev libkf5wayland-dev git gettext libkf5archive-dev libkf5notifications-dev libxcb-util0-dev libsm-dev libkf5crash-dev libkf5newstuff-dev libxcb-shape0-dev libxcb-randr0-dev libx11-dev libx11-xcb-dev -y
+    sudo git clone https://github.com/KDE/latte-dock.git /usr/local/latte-dock
+    cd /usr/local/latte-dock && sudo sh install.sh
+
+    echo 'Installing Kvantum Manager'
+    sudo add-apt-repository ppa:papirus/papirus -y
+    sudo apt-get update && sudo apt install qt5-style-kvantum -y
+fi
+
+echo "Setting up your git global user name and email"
+git config --global user.name "$git_config_user_name"
+git config --global user.email $git_config_user_email
+
+echo 'Cloning your .gitconfig from gist'
+getmy .gitconfig
+
+echo 'Generating a SSH Key'
+ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+
+
+echo 'Installing FiraCode'
+sudo apt-get install fonts-firacode -y
+
+echo 'Installing NVM' 
+sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
+
+export NVM_DIR="$HOME/.nvm" && (
+git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+cd "$NVM_DIR"
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+source ~/.zshrc
+clear
+
+echo 'Installing NodeJS LTS'
+nvm --version
+nvm install --lts
+nvm current
+
+echo 'Installing Yarn'
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install --no-install-recommends yarn
+echo '"--emoji" true' >> ~/.yarnrc
+
+echo 'Installing VSCode'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https -y
+sudo apt-get update && sudo apt-get install code -y
+
+echo 'Installing Vivaldi' 
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
+sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main' -y
+sudo apt update && sudo apt install vivaldi-stable
+
+echo 'Installing chrome' 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+echo 'Launching Vivaldi on Github so you can paste your keys'
+vivaldi https://github.com/settings/keys </dev/null >/dev/null 2>&1 & disown
+
+echo 'Installing Docker'
+sudo apt-get purge docker docker-engine docker.io
+sudo apt-get install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo chmod 777 /var/run/docker.sock
+
+echo 'Installing docker-compose'
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+
+echo 'Installing VLC'
+sudo apt-get install vlc -y
+sudo apt-get install vlc-plugin-access-extra libbluray-bdj libdvdcss2 -y
+
+echo 'Installing Discord'
+wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo dpkg -i discord.deb
+sudo apt-get install -f -y && rm discord.deb
+
+
+echo 'Install Github CLI'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
+
+
+# echo 'Installing Lotion'
+# sudo git clone https://github.com/puneetsl/lotion.git /usr/local/lotion
+# cd /usr/local/lotion && sudo ./setup.sh install
+
+echo 'Updating and Cleaning Unnecessary Packages'
+sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get full-upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
+clear
+
+echo 'Bumping the max file watchers'
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+
+
+echo 'All setup, enjoy!'
